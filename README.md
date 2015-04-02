e81ee6d7e7c45fc60a5ebec78b69ee057fcc98ae
Tina Stith-Twine <tinatwine@gmail.com>
2015-04-01 20:48:19 -0400

    add welcome index

--- README.md
+++ README.md
@@ -2,4 +2,3 @@
 This is a sample runner's app which list the runner's distances (date and time).
 
 Within the app, the runner can create, update, edit or destroy their distance.
-
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
 
+  root 'welcome#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'distances#index'
+   # root 'distances#index'
    
     #resources :runners, except: [:edit, :update] do
-      resources :distances 
+  resources :distances 
  
   
 
